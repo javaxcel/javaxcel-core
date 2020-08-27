6d64534362990bbcdecce8813ce701968810bbc9
ImSejin <hackeres123@gmail.com>
2020-08-27 17:24:34 +0900

    🛠️ Update: utilities

--- src/main/java/com/github/javaxcel/util/TypeClassifier.java
+++ src/main/java/com/github/javaxcel/util/TypeClassifier.java
@@ -6,20 +6,30 @@ public final class TypeClassifier {
 
     private static final Class<?>[] PRIMITIVE_TYPES = {byte.class, short.class, int.class, long.class, float.class, double.class, char.class, boolean.class};
     private static final Class<?>[] WRAPPER_TYPES = {Byte.class, Short.class, Integer.class, Long.class, Float.class, Double.class, Character.class, Boolean.class};
+    private static final Class<?>[] NUMBER_TYPES = {byte.class, short.class, int.class, long.class, float.class, double.class,
+            Byte.class, Short.class, Integer.class, Long.class, Float.class, Double.class};
+
+    public static boolean isNumeric(Class<?> clazz) {
+        for (Class<?> type : NUMBER_TYPES) {
+            if (clazz.equals(type)) return true;
+        }
+
+        return false;
+    }
 
     /**
      *
      * @param clazz class
      * @return if the class is writable with excel or not.
      */
-    public static boolean isWritableClass(Class<?> clazz) {
-        return isStringClass(clazz) || isPrimitive(clazz) || isWrapperClass(clazz);
+    public static boolean isWritable(Class<?> clazz) {
+        return isString(clazz) || isPrimitive(clazz) || isWrapper(clazz);
     }
 
     /**
      * 자료형이 문자열(java.lang.String)인지 확인한다.
      */
-    public static boolean isStringClass(Class<?> clazz) {
+    public static boolean isString(Class<?> clazz) {
         return clazz.equals(String.class);
     }
 
@@ -37,7 +47,7 @@ public final class TypeClassifier {
     /**
      * 자료형이 래퍼클래스인지 확인한다.
      */
-    public static boolean isWrapperClass(Class<?> clazz) {
+    public static boolean isWrapper(Class<?> clazz) {
         for (Class<?> type : WRAPPER_TYPES) {
             if (clazz.equals(type)) return true;
         }
