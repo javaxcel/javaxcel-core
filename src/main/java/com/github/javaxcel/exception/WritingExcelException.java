abc02b22b992e24f3792b1a640c914da73e8761a
ImSejin <hackeres123@gmail.com>
2020-10-15 01:06:09 +0900

    ✏️ Correct: typos

--- src/main/java/com/github/javaxcel/exception/WritingExcelException.java
+++ src/main/java/com/github/javaxcel/exception/WritingExcelException.java
@@ -3,7 +3,7 @@ package com.github.javaxcel.exception;
 public class WritingExcelException extends RuntimeException {
 
     public WritingExcelException() {
-        super("Failed to write data in to the excel sheet");
+        super("Failed to write data to the excel sheet");
     }
 
     public WritingExcelException(String message) {
