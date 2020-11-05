51f81e07d292ab53319ca369e07951d8a49b1474
ImSejin <hackeres123@gmail.com>
2020-11-05 12:29:41 +0900

    🚚 Move: methods in `ExcelStyler` to `ExcelUtils`

--- src/main/java/com/github/javaxcel/util/ExcelUtils.java
+++ src/main/java/com/github/javaxcel/util/ExcelUtils.java
@@ -99,4 +99,67 @@ public final class ExcelUtils {
         return cell instanceof HSSFCell;
     }
 
+    /**
+     * Adjusts width of columns to fit the contents.
+     *
+     * <p> This can be affected by font size and font family.
+     * If you want this process well, set up the same font family into all cells.
+     * This process will be perform in parallel.
+     *
+     * @param sheet        excel sheet
+     * @param numOfColumns number of the columns that wanted to make fit contents.
+     * @see Sheet#autoSizeColumn(int)
+     */
+    public static void autoResizeColumns(Sheet sheet, int numOfColumns) {
+        IntStream.range(0, numOfColumns).parallel().forEach(sheet::autoSizeColumn);
+    }
+
+    /**
+     * Hides extraneous rows.
+     *
+     * <p> This process must not be performed in parallel.
+     * If try it, this will throw {@link java.util.ConcurrentModificationException}.
+     *
+     * @param sheet     excel sheet
+     * @param numOfRows number of the rows that have contents.
+     * @see Row#setZeroHeight(boolean)
+     */
+    public static void hideExtraRows(Sheet sheet, int numOfRows) {
+        final int maxRows = getMaxRows(sheet);
+
+        for (int i = numOfRows; i < maxRows; i++) {
+            Row row = sheet.getRow(i);
+            if (row == null) row = sheet.createRow(i);
+
+            row.setZeroHeight(true);
+        }
+    }
+
+    /**
+     * Hides extraneous columns.
+     *
+     * <p> This process shouldn't be performed in parallel.
+     * If try it, this is about 46% slower when handled in parallel
+     * than when handled in sequential.
+     *
+     * <pre>{@code
+     *     +------------+----------+
+     *     | sequential | parallel |
+     *     +------------+----------+
+     *     | 15s        | 22s      |
+     *     +------------+----------+
+     * }</pre>
+     *
+     * @param sheet        excel sheet
+     * @param numOfColumns number of the columns that have contents.
+     * @see Sheet#setColumnHidden(int, boolean)
+     */
+    public static void hideExtraColumns(Sheet sheet, int numOfColumns) {
+        final int maxColumns = getMaxColumns(sheet);
+
+        for (int i = numOfColumns; i < maxColumns; i++) {
+            sheet.setColumnHidden(i, true);
+        }
+    }
+
 }
