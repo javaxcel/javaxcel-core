c69505009700acf9a9a4246b9a30e89ab8bead76
ImSejin <hackeres123@gmail.com>
2020-11-07 21:18:53 +0900

    🧪 Update: test code

--- src/test/java/com/github/javaxcel/in/MapReaderTest.java
+++ src/test/java/com/github/javaxcel/in/MapReaderTest.java
@@ -3,9 +3,11 @@ package com.github.javaxcel.in;
 import com.github.javaxcel.factory.ExcelReaderFactory;
 import com.github.javaxcel.factory.ExcelWriterFactory;
 import com.github.javaxcel.model.Mockables;
+import com.github.javaxcel.util.ExcelUtils;
 import io.github.imsejin.common.tool.Stopwatch;
 import lombok.Cleanup;
 import lombok.SneakyThrows;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.AfterEach;
@@ -15,6 +17,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import javax.annotation.Nonnull;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Path;
 import java.util.Arrays;
@@ -56,10 +59,10 @@ public class MapReaderTest {
         stopWatch.start(String.format("create '%s' file", filename));
         File file = new File(path.toFile(), filename);
         @Cleanup FileOutputStream out = new FileOutputStream(file);
-        @Cleanup Workbook workbook = new XSSFWorkbook();
+        @Cleanup Workbook workbook = new HSSFWorkbook();
         stopWatch.stop();
 
-        int numOfMocks = 10_000;
+        int numOfMocks = ExcelUtils.getMaxRows(workbook) + 10_000;
         stopWatch.start(String.format("create %,d mocks", numOfMocks));
         List<Map<String, Object>> maps = IntStream.range(0, numOfMocks)
                 .mapToObj(i -> getRandomMap(keys)).collect(toList());
@@ -71,7 +74,8 @@ public class MapReaderTest {
 
         // when
         stopWatch.start(String.format("read %,d maps", numOfMocks));
-        List<Map<String, Object>> actual = ExcelReaderFactory.create(workbook).read();
+        @Cleanup Workbook wb = new HSSFWorkbook(new FileInputStream(file));
+        List<Map<String, Object>> actual = ExcelReaderFactory.create(wb).read();
         stopWatch.stop();
 
         // then
