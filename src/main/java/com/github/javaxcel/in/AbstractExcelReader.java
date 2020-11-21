ac6d93eccf6dedfa03c85d238b0ee3793454f7b6
ImSejin <hackeres123@gmail.com>
2020-11-22 01:12:57 +0900

    🚿 Clean up: code

--- src/main/java/com/github/javaxcel/in/AbstractExcelReader.java
+++ src/main/java/com/github/javaxcel/in/AbstractExcelReader.java
@@ -1,7 +1,7 @@
 package com.github.javaxcel.in;
 
-import com.github.javaxcel.converter.in.BasicReadingConverter;
 import com.github.javaxcel.util.ExcelUtils;
+import io.github.imsejin.common.util.StringUtils;
 import org.apache.poi.ss.usermodel.*;
 
 import java.lang.reflect.Field;
@@ -126,7 +126,7 @@ public abstract class AbstractExcelReader<W extends Workbook, T> implements Exce
             String cellValue = dataFormatter.formatCellValue(cell, this.formulaEvaluator);
 
             // Converts empty string to null because when CellType is BLANK, DataFormatter returns empty string.
-            map.put(getColumnName(cell, i), cellValue.equals("") ? null : cellValue);
+            map.put(getColumnName(cell, i), StringUtils.ifNullOrEmpty(cellValue, (String) null));
         }
 
         // Increase count of reading row.
