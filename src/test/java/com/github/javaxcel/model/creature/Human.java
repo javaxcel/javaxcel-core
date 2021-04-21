4c42f5887b049a0805887833deb9ae4324f34320
ImSejin <hackeres123@gmail.com>
2021-04-21 14:34:32 +0900

    🧪 Add: test case

--- src/test/java/com/github/javaxcel/model/creature/Human.java
+++ src/test/java/com/github/javaxcel/model/creature/Human.java
@@ -39,7 +39,7 @@ public class Human extends Creature implements Mockables<Human> {
     @ExcelColumn(name = "Place of Birth")
     @ExcelWriterExpression("T(com.github.javaxcel.Converter).capitalize(#placeOfBirth, '-')")
     @ExcelReaderExpression("#placeOfBirth?.toLowerCase()") // null-safe operator '?.'
-    private String placeOfBirth;
+    private UUID placeOfBirth;
 
     @ExcelColumn(name = "Rest Seconds of Life")
     @ExcelWriterExpression("#restSecondsOfLife + ' sec'")
@@ -78,7 +78,7 @@ public class Human extends Creature implements Mockables<Human> {
     private boolean disabled;
 
     public Human(Kingdom kingdom, Sex sex, int lifespan, String name, LocalDate birthday, LocalTime birthTime,
-                 String placeOfBirth, BigDecimal restSecondsOfLife, BigInteger numOfCells, float height, float weight,
+                 UUID placeOfBirth, BigDecimal restSecondsOfLife, BigInteger numOfCells, float height, float weight,
                  int[] agesFromBirthToPuberty, int[] agesFromTwilightToDeath, boolean disabled) {
         super(kingdom, sex, lifespan);
         this.name = name;
@@ -98,19 +98,19 @@ public class Human extends Creature implements Mockables<Human> {
     public List<Human> createDesignees() {
         return Arrays.asList(
                 new Human(Kingdom.ANIMALIA, Sex.MALE, 30, "Jeremy", LocalDate.now(), LocalTime.now(),
-                        "seoul,-republic-of-korea", new BigDecimal("34857058102347105675.583417804735034534756348701"),
+                        UUID.randomUUID(), new BigDecimal("34857058102347105675.583417804735034534756348701"),
                         new BigInteger("3024563964348504345428940615799280516897078902523043244"), 180, 70,
                         new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, new int[]{30}, true),
                 new Human(Kingdom.ARCHAEA, Sex.INTERSEX, 100, "germ", LocalDate.now(), LocalTime.now(),
-                        "the-earth", new BigDecimal("95230947174310317483424235.32758234"),
+                        UUID.randomUUID(), new BigDecimal("95230947174310317483424235.32758234"),
                         new BigInteger("232549312541241462344"), 1, 1,
                         new int[]{0, 1}, new int[]{99, 100}, false),
                 new Human(Kingdom.BACTERIA, Sex.INTERSEX, 47_000, "virus", LocalDate.now(), LocalTime.now(),
-                        "the-earth", new BigDecimal("1679811295023592390682741892423423905802.69345023"),
+                        UUID.randomUUID(), new BigDecimal("1679811295023592390682741892423423905802.69345023"),
                         new BigInteger("672586170780398545235540893463155661323"), 1, 1,
                         new int[]{0}, new int[]{}, false),
                 new Human(Kingdom.PLANTAE, Sex.FEMALE, 2000, "tree of life", LocalDate.now(), LocalTime.now(),
-                        "united-state-of-america", new BigDecimal("728349210342742.2346791209564390683103567314567813420124892047128537183"),
+                        UUID.randomUUID(), new BigDecimal("728349210342742.2346791209564390683103567314567813420124892047128537183"),
                         new BigInteger("13489570439503567143859483067247856304724853452034"), 1000, 10_000,
                         new int[]{0, 1, 2, 3, 4, 5, 6}, new int[]{1996, 1997, 1998, 1999, 2000}, false)
         );
@@ -128,7 +128,7 @@ public class Human extends Creature implements Mockables<Human> {
             String name = RANDOM.nextDouble() <= 0.85 ? Mockables.generateRandomText(RANDOM.nextInt(24) + 1) : null;
             LocalDate birthday = LocalDate.now();
             LocalTime birthTime = LocalTime.now().withNano(123_000_000); // with 123 ms
-            String placeOfBirth = name == null ? null : UUID.randomUUID().toString();
+            UUID placeOfBirth = name == null ? null : UUID.randomUUID();
 
             sb.setLength(0);
             sb.append(RANDOM.nextInt(Integer.MAX_VALUE - 10_000_000) + 10_000_000);
