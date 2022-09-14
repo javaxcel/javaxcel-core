ef832b5d62c1d83c65eb676ccf19d22527b1c560
imsejin <hackeres123@gmail.com>
2022-09-14 14:58:11 +0900

    Add: supported handlers for `Year`, `YearMonth`, `Month`, `MonthDay`, `Instant` by default

--- src/main/java/com/github/javaxcel/converter/handler/registry/impl/DefaultExcelTypeHandlerRegistry.java
+++ src/main/java/com/github/javaxcel/converter/handler/registry/impl/DefaultExcelTypeHandlerRegistry.java
@@ -26,12 +26,15 @@ import com.github.javaxcel.converter.handler.impl.DoubleTypeHandler;
 import com.github.javaxcel.converter.handler.impl.EnumTypeHandler;
 import com.github.javaxcel.converter.handler.impl.FileTypeHandler;
 import com.github.javaxcel.converter.handler.impl.FloatTypeHandler;
+import com.github.javaxcel.converter.handler.impl.InstantTypeHandler;
 import com.github.javaxcel.converter.handler.impl.IntegerTypeHandler;
 import com.github.javaxcel.converter.handler.impl.LocalDateTimeTypeHandler;
 import com.github.javaxcel.converter.handler.impl.LocalDateTypeHandler;
 import com.github.javaxcel.converter.handler.impl.LocalTimeTypeHandler;
 import com.github.javaxcel.converter.handler.impl.LocaleTypeHandler;
 import com.github.javaxcel.converter.handler.impl.LongTypeHandler;
+import com.github.javaxcel.converter.handler.impl.MonthDayTypeHandler;
+import com.github.javaxcel.converter.handler.impl.MonthTypeHandler;
 import com.github.javaxcel.converter.handler.impl.OffsetDateTimeTypeHandler;
 import com.github.javaxcel.converter.handler.impl.OffsetTimeTypeHandler;
 import com.github.javaxcel.converter.handler.impl.PathTypeHandler;
@@ -40,6 +43,8 @@ import com.github.javaxcel.converter.handler.impl.StringTypeHandler;
 import com.github.javaxcel.converter.handler.impl.URITypeHandler;
 import com.github.javaxcel.converter.handler.impl.URLTypeHandler;
 import com.github.javaxcel.converter.handler.impl.UUIDTypeHandler;
+import com.github.javaxcel.converter.handler.impl.YearMonthTypeHandler;
+import com.github.javaxcel.converter.handler.impl.YearTypeHandler;
 import com.github.javaxcel.converter.handler.impl.ZonedDateTimeTypeHandler;
 
 public class DefaultExcelTypeHandlerRegistry extends ExcelTypeHandlerRegistryImpl {
@@ -73,12 +78,17 @@ public class DefaultExcelTypeHandlerRegistry extends ExcelTypeHandlerRegistryImp
         add(new UUIDTypeHandler());
         add(new LocaleTypeHandler());
         // java.time
+        add(new YearTypeHandler());
+        add(new YearMonthTypeHandler());
+        add(new MonthTypeHandler());
+        add(new MonthDayTypeHandler());
         add(new LocalTimeTypeHandler());
         add(new LocalDateTypeHandler());
         add(new LocalDateTimeTypeHandler());
         add(new ZonedDateTimeTypeHandler());
         add(new OffsetTimeTypeHandler());
         add(new OffsetDateTimeTypeHandler());
+        add(new InstantTypeHandler());
         // java.net
         add(new URITypeHandler());
         add(new URLTypeHandler());
